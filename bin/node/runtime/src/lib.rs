82651e8681c448823d8da161464aab64c71c8b90
hskang9 <hskang9@gmail.com>
2020-11-18 21:57:47 +0900

    fix fmt

--- bin/node/runtime/src/lib.rs
+++ bin/node/runtime/src/lib.rs
@@ -3,7 +3,6 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 // `construct_runtime!` does a lot of recursion and requires us to increase the limit to 256.
 #![recursion_limit = "256"]
-use sp_std::prelude::*;
 use codec::{Decode, Encode};
 use frame_support::{
     construct_runtime, debug, parameter_types,

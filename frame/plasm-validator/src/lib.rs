809db81b8610e0d03303d0c0626f622f23ef8b36
hskang9 <hskang9@gmail.com>
2020-12-25 23:17:17 +0900

    add staking ledger

--- frame/plasm-validator/src/lib.rs
+++ frame/plasm-validator/src/lib.rs
@@ -4,7 +4,7 @@
 #![cfg_attr(not(feature = "std"), no_std)]
  
 use frame_support::{
-   decl_event, decl_module, decl_storage,
+   decl_event, decl_module, decl_storage, decl_error,
    traits::{Currency, Imbalance, LockableCurrency, OnUnbalanced, Time},
    StorageMap, StorageValue,
 };
@@ -13,12 +13,11 @@ use frame_system::{
 	offchain::SendTransactionTypes,
 };
 use pallet_plasm_rewards::{
-   traits::{ComputeEraWithParam, EraFinder, ForSecurityEraRewardFinder, MaybeValidators},
-   EraIndex,
+   traits::{ComputeEraWithParam, EraFinder, ForSecurityEraRewardFinder, MaybeValidators}, EraIndex
 };
 use sp_runtime::{
-   traits::{Saturating, Zero},
-   Perbill, RuntimeDebug
+   traits::{Saturating, Zero, AtLeast32BitUnsigned},
+   Perbill, RuntimeDebug, 
 };
 pub use sp_staking::SessionIndex;
 use sp_std::{prelude::*, vec::Vec};
@@ -32,6 +31,7 @@ mod compute_era;
 pub use compute_era::*;
 
 use codec::{HasCompact, Encode, Decode};
+
   
 pub type BalanceOf<T> =
    <<T as Trait>::Currency as Currency<<T as system::Trait>::AccountId>>::Balance;
@@ -87,6 +87,146 @@ impl Default for ValidatorPrefs {
        }
    }
 }
+
+/// Just a Balance/BlockNumber tuple to encode when a chunk of funds will be unlocked.
+#[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug)]
+pub struct UnlockChunk<Balance: HasCompact> {
+	/// Amount of funds to be unlocked.
+	#[codec(compact)]
+	value: Balance,
+	/// Era number at which point it'll be unlocked.
+	#[codec(compact)]
+	era: EraIndex,
+}
+
+/// The ledger of a (bonded) stash.
+#[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug)]
+pub struct StakingLedger<AccountId, Balance: HasCompact> {
+	/// The stash account whose balance is actually locked and at stake.
+	pub stash: AccountId,
+	/// The total amount of the stash's balance that we are currently accounting for.
+	/// It's just `active` plus all the `unlocking` balances.
+	#[codec(compact)]
+	pub total: Balance,
+	/// The total amount of the stash's balance that will be at stake in any forthcoming
+	/// rounds.
+	#[codec(compact)]
+	pub active: Balance,
+	/// Any balance that is becoming free, which may eventually be transferred out
+	/// of the stash (assuming it doesn't get slashed first).
+	pub unlocking: Vec<UnlockChunk<Balance>>,
+	/// List of eras for which the stakers behind a validator have claimed rewards. Only updated
+	/// for validators.
+	pub claimed_rewards: Vec<EraIndex>,
+}
+
+impl<
+	AccountId,
+	Balance: HasCompact + Copy + Saturating + AtLeast32BitUnsigned,
+> StakingLedger<AccountId, Balance> {
+	/// Remove entries from `unlocking` that are sufficiently old and reduce the
+	/// total by the sum of their balances.
+	fn consolidate_unlocked(self, current_era: EraIndex) -> Self {
+		let mut total = self.total;
+		let unlocking = self.unlocking.into_iter()
+			.filter(|chunk| if chunk.era > current_era {
+				true
+			} else {
+				total = total.saturating_sub(chunk.value);
+				false
+			})
+			.collect();
+
+		Self {
+			stash: self.stash,
+			total,
+			active: self.active,
+			unlocking,
+			claimed_rewards: self.claimed_rewards
+		}
+	}
+
+	/// Re-bond funds that were scheduled for unlocking.
+	fn rebond(mut self, value: Balance) -> Self {
+		let mut unlocking_balance: Balance = Zero::zero();
+
+		while let Some(last) = self.unlocking.last_mut() {
+			if unlocking_balance + last.value <= value {
+				unlocking_balance += last.value;
+				self.active += last.value;
+				self.unlocking.pop();
+			} else {
+				let diff = value - unlocking_balance;
+
+				unlocking_balance += diff;
+				self.active += diff;
+				last.value -= diff;
+			}
+
+			if unlocking_balance >= value {
+				break
+			}
+		}
+
+		self
+	}
+}
+
+impl<AccountId, Balance> StakingLedger<AccountId, Balance> where
+	Balance: AtLeast32BitUnsigned + Saturating + Copy,
+{
+	/// Slash the validator for a given amount of balance. This can grow the value
+	/// of the slash in the case that the validator has less than `minimum_balance`
+	/// active funds. Returns the amount of funds actually slashed.
+	///
+	/// Slashes from `active` funds first, and then `unlocking`, starting with the
+	/// chunks that are closest to unlocking.
+	fn slash(
+		&mut self,
+		mut value: Balance,
+		minimum_balance: Balance,
+	) -> Balance {
+		let pre_total = self.total;
+		let total = &mut self.total;
+		let active = &mut self.active;
+
+		let slash_out_of = |
+			total_remaining: &mut Balance,
+			target: &mut Balance,
+			value: &mut Balance,
+		| {
+			let mut slash_from_target = (*value).min(*target);
+
+			if !slash_from_target.is_zero() {
+				*target -= slash_from_target;
+
+				// don't leave a dust balance in the staking system.
+				if *target <= minimum_balance {
+					slash_from_target += *target;
+					*value += sp_std::mem::replace(target, Zero::zero());
+				}
+
+				*total_remaining = total_remaining.saturating_sub(slash_from_target);
+				*value -= slash_from_target;
+			}
+		};
+
+		slash_out_of(total, active, &mut value);
+
+		let i = self.unlocking.iter_mut()
+			.map(|chunk| {
+				slash_out_of(total, &mut chunk.value, &mut value);
+				chunk.value
+			})
+			.take_while(|value| value.is_zero()) // take all fully-consumed chunks out.
+			.count();
+
+		// kill all drained chunks.
+		let _ = self.unlocking.drain(..i);
+
+		pre_total.saturating_sub(*total)
+	}
+}
  
 /// A record of the nominations made by a specific account.
 #[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug)]
@@ -109,17 +249,23 @@ decl_storage! {
        /// The already untreated era is EraIndex.
        pub UntreatedEra get(fn untreated_era): EraIndex;
  
+       
+       /// Map from all (unlocked) "controller" accounts to the info regarding the staking.
+	   pub Ledger get(fn ledger):
+       map hasher(blake2_128_concat) T::AccountId
+       => Option<StakingLedger<T::AccountId, BalanceOf<T>>>;
+
        /// The currently elected validator set keyed by stash account ID.
        pub ElectedValidators get(fn elected_validators):
            map hasher(twox_64_concat) EraIndex => Option<Vec<T::AccountId>>;
  
-       /// The map from nominator stash key to the set of stash keys of all validators to nominate.
-       pub Nominators get(fn nominators):
-       map hasher(twox_64_concat) T::AccountId => Option<Nominations<T::AccountId>>;
- 
        /// The map from (wannabe) validator stash key to the preferences of that validator.
        pub Validators get(fn validators):
        map hasher(twox_64_concat) T::AccountId => ValidatorPrefs;
+
+       /// The map from nominator stash key to the set of stash keys of all validators to nominate.
+       pub Nominators get(fn nominators):
+       map hasher(twox_64_concat) T::AccountId => Option<Nominations<T::AccountId>>;
 
        /// Set of next era accounts that can validate blocks.
        pub ValidatorsList get(fn validators_list) config(): Vec<T::AccountId>;
@@ -200,6 +346,15 @@ decl_event!(
        TotalValidatorRewards(EraIndex, Balance),
    }
 );
+
+decl_error!{
+    pub enum Error for Module<T: Trait> {
+        /// Not a controller account.
+        NotController,
+        /// The call is not allowed at the given time due to restrictions of election period.
+		CallNotAllowed,
+    }
+}
  
 impl<T: Trait> Module<T> {
    pub fn reward_to_validators(era: &EraIndex, max_payout: &BalanceOf<T>) -> BalanceOf<T> {
